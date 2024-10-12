1607af82d9c29d4fccc2f8ecda365d3d363a8cac
soutAhmedTayseer <122810273+soutAhmedTayseer@users.noreply.github.com>
2024-10-12 18:23:35 +0300

    onboarding and login screens

---
+++ lib/on_boarding_screen.dart
@@ -0,0 +1,132 @@
+import 'package:flutter/material.dart';
+import 'package:shop_app/components.dart';
+import 'package:shop_app/login_screen.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+
+class BoardingModel {
+  final String image;
+  final String title;
+  final String body;
+
+  BoardingModel({required this.image, required this.title, required this.body});
+}
+
+class OnBoardingScreen extends StatefulWidget {
+  const OnBoardingScreen({super.key});
+
+  @override
+  State<OnBoardingScreen> createState() => _OnBoardingScreenState();
+}
+
+var boardController = PageController();
+
+class _OnBoardingScreenState extends State<OnBoardingScreen> {
+  List<BoardingModel> boarding = [
+    BoardingModel(image: 'assets/images/1.png', title: '1', body: '1'),
+    BoardingModel(image: 'assets/images/2.png', title: '2', body: '2'),
+    BoardingModel(image: 'assets/images/3.png', title: '3', body: '3')
+  ];
+
+  bool isLast = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          TextButton(
+            onPressed: () {
+              navigateandfinish(context, LoginScreen());
+            },
+            child: const Text('Skip',style: TextStyle(color: Colors.blue,fontWeight: FontWeight.bold,fontSize: 20),),
+          ),
+        ],
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              Expanded(
+                child: PageView.builder(
+                  controller: boardController,
+                  physics: const BouncingScrollPhysics(),
+                  itemBuilder: (context, index) =>
+                      buildBoardingItem(boarding[index]),
+                  itemCount: boarding.length,
+                  onPageChanged: (int index) {
+                    if (index == boarding.length - 1) {
+                      setState(() {
+                        isLast = true;
+                      });
+                    } else {
+                      setState(() {
+                        isLast = false;
+                      });
+                    }
+                  },
+                ),
+              ),
+              Row(
+                children: [
+                  SmoothPageIndicator(
+                    controller: boardController,
+                    effect: const ExpandingDotsEffect(
+                      dotColor: Colors.grey,
+                      spacing: 3,
+                      dotWidth: 13,
+                      expansionFactor: 4,
+                      activeDotColor: Colors.blue,
+                    ),
+                    count: boarding.length,
+                  ),
+                  const Spacer(),
+                  FloatingActionButton(
+                    onPressed: () {
+                      if (isLast) {
+                        navigateandfinish(context, LoginScreen());
+                      } else {
+                        boardController.nextPage(
+                          duration: const Duration(milliseconds: 750),
+                          curve: Curves.fastLinearToSlowEaseIn,
+                        );
+                      }
+                    },
+                    child: const Icon(Icons.navigate_next_outlined),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+Widget buildBoardingItem(BoardingModel model) => Padding(
+  padding: const EdgeInsets.all(20.0),
+  child: Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    mainAxisAlignment: MainAxisAlignment.start,
+    children: [
+      Expanded(
+        child: Center(
+          child: Image(
+            image: AssetImage(model.image),
+          ),
+        ),
+      ),
+      const SizedBox(height: 20),
+      Text(
+        model.title,
+        style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+      ),
+      const SizedBox(height: 20),
+      Text(
+        model.body,
+        style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+      ),
+    ],
+  ),
+);
